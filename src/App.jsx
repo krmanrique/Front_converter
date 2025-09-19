c5d58583db71ebb65d5aa321b093fc68d42ddfc1
Krmanrique <krmanriquesanchez@gmail.com>
2025-09-18 19:42:24 -0500

    correccion2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Converter from './components/converter'
+import Converter from './components/Converter'
 
 function App() {
   const [count, setCount] = useState(0)
